56658ea65f5b754a90b764a2af6fd68a88a5c26e
Bill Kerr <bill@tailwindapp.com>
2024-07-04 21:05:36 -0500

    Add types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,18 @@
-declare module "@tailwind/jsdom";
+/// <reference path="base.d.ts"/>
+
+// eslint-disable-next-line @definitelytyped/no-declare-current-package
+declare module "@tailwind/jsdom" {
+  interface DOMWindow {
+    FinalizationRegistry: FinalizationRegistryConstructor;
+    WeakRef: WeakRefConstructor;
+    InputEvent: typeof InputEvent;
+    External: typeof External;
+  }
+}
+
+// Necessary to avoid breaking dependents because of the dependency
+// on the `ESNext.WeakRef` lib:
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface FinalizationRegistryConstructor {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface WeakRefConstructor {}
